fa856be30df7aeb383fb29c48150964bf8bb13e3
Daniele <lubrano93@gmail.com>
2024-08-24 15:52:21 +0300

    Capture active window instead of entire screen

--- main.py
+++ main.py
@@ -4,22 +4,28 @@ import pyautogui
 import time
 
 
-def capture_screen() -> np.ndarray:
-    screenshot = pyautogui.screenshot()
+def capture_active_window() -> np.ndarray:
+    window = pyautogui.getActiveWindow()
+    if window is None:
+        raise Exception("No active window found")
+
+    screenshot = pyautogui.screenshot(
+        region=(window.left, window.top, window.width, window.height)
+    )
     screenshot_np = np.array(screenshot)
     return cv2.cvtColor(screenshot_np, cv2.COLOR_RGB2BGR)
 
 
 def detect_golden_cookie(
     screen: np.ndarray, template: np.ndarray, confidence_threshold=0.5
-) -> float:
+) -> bool:
     result = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
     return np.any(result >= confidence_threshold)
 
 
 while True:
     golden_cookie_image = cv2.imread("golden_cookie.jpg", cv2.IMREAD_UNCHANGED)
-    screen = capture_screen()
+    screen = capture_active_window()
     if detect_golden_cookie(screen, golden_cookie_image):
         print("Found a golden cookie")
         time.sleep(10)
